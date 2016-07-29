8a8bfa2190c5f85f3917e39d42c7c00a3a228d1c
pietrzakacper <kacpietrzak@gmail.com>
2016-07-29 16:37:05 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function showResults(response) {
+    $(".result-container").empty();
+    $.each(response[1], function(i, item) {
+        $(".result-container").append(
+            '<a href="' + response[3][i] + '" target="_blank"><div class="result-item">' + "<strong><p>" + item + "</p></strong>" + "<div>" + response[2][i] + "</div>" + "</div></a>"
+        );
+    });
+}
+
+function showLoadAnim() {
+    $(".result-container").html('<div class="sk-folding-cube"><div class="sk-cube1 sk-cube"></div><div class="sk-cube2 sk-cube"></div><div class="sk-cube4 sk-cube"></div><div class="sk-cube3 sk-cube"></div></div>');
+}
+
+function submitFunction() {
+    $.ajax({
+        crossOrigin: true,
+        data: {
+            action: "opensearch",
+            type: "json",
+            search: $("#search").val(),
+        },
+        dataType: "jsonp",
+        cache: false,
+        url: "https://en.wikipedia.org/w/api.php",
+        jsonpCallback: "showResults"
+    })
+    showLoadAnim();
+}
+
+$("#search-form").submit(function(e) {
+    e.preventDefault();
+    submitFunction();
+});
